5431f52b5a7e117aae6a05f4f37a5f54f980d8d2
tampc <tamphanpw1@gmail.com>
2019-06-17 11:08:09 +0700

    update

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tapha-shop'));
+app.use(express.static(__dirname + '/dist/MyAngularApp-publish3'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/tapha-shop/index.html'));
+res.sendFile(path.join(__dirname+'/dist/MyAngularApp-publish3/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
